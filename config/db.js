885b5f58b8261bd2ccb8b96b492850423b9a8091
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-10-16 13:13:40 -0400

    setup remote database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-  URI: "mongodb://localhost/user",
+  // URI: "mongodb://localhost/user",
+  URI: "mongodb+srv://jackycheung02:hodra5-fupdyr-qapKig@cluster0.waxrkwp.mongodb.net/?retryWrites=true&w=majority",
   Secret: "SomeSecret",
 };
