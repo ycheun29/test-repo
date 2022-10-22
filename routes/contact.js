a8a232cbf14588bbe36a70fadf3a38b5fbd1a693
ycheun29 <ycheun29@my.centennialcollege.ca>
2022-10-22 15:18:28 -0400

    update comments

--- routes/contact.js
+++ routes/contact.js
@@ -23,22 +23,11 @@ function requireAuth(req, res, next) {
   next();
 }
 
-/* GET Route for the Contact List page - READ Operation */
 router.get("/", requireAuth, contactController.displayContactList);
-
-/* GET Route for displaying the Add page - CREATE Operation */
 router.get("/add", requireAuth, contactController.displayAddPage);
-
-/* POST Route for processing the Add page - CREATE Operation */
 router.post("/add", requireAuth, contactController.processAddPage);
-
-/* GET Route for displaying the Edit page - UPDATE Operation */
 router.get("/edit/:id", requireAuth, contactController.displayEditPage);
-
-/* POST Route for processing the Edit page - UPDATE Operation */
 router.post("/edit/:id", requireAuth, contactController.processEditPage);
-
-/* GET to perform  Deletion - DELETE Operation */
 router.get("/delete/:id", requireAuth, contactController.performDelete);
 
 module.exports = router;
